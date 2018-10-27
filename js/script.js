16bf7f50814f8f8319e185c58471e2708b9ae2ca
ErickFoxes <37157947+ErickFoxes@users.noreply.github.com>
2018-10-26 18:37:56 -0600

    Cambio

--- js/script.js
+++ js/script.js
@@ -1,101 +1,64 @@
-// Object Literal
-var person = {
-    /* property: value*/
-    name: "Néstor",
-    lastname: "Aldana",
-    birthday: Date.now()
-}; // JSON (JavaScript Object Notation)
+window.onload = init;
 
-// Access to propertys
-console.log(person.name);
-// Change object's property value
-person.birthday = new Date(1994, 0, 11); // 11 - Jan - 1994
-console.log(person.birthday);
+function init() {
+    let todoList = {
+        listHTML: document.getElementById("todoList"),
+        listTask: [],
+        add(task, priority = false) {
+            let element = document.createElement("li");
+            element.innerText = task;
+            let eliminarBtn = document.createElement("input");
+            let realizadoBtn = document.createElement("input");
+            eliminarBtn.type = "button";
+            eliminarBtn.value = "Eliminar";
+            realizadoBtn.type = "button";
+            realizadoBtn.value = "Realizado";
+            element.appendChild(eliminarBtn);
+            element.appendChild(realizadoBtn);                 
 
-console.log(person.dui) // undefined
-person.dui = "000000000" // Assign 
-console.log(person.dui) // 000000000
+            // Elimine de la lista
+            eliminarBtn.addEventListener("click", function () {
+                let parent = element.parentNode;
+                if(parent){
+                    parent.removeChild(element);
+                }
+            });
 
-// Example
-function createPerson(name, lastname, birthday, dui) {
-    return {
-        name,
-        lastname,
-        birthday,
-        dui
-    }
-}
+            realizadoBtn.addEventListener("click", function () {
+                let hecho = document.createElement("p");
+                hecho.innerText = " *Realizado* ";
+                hecho.style.color = "red";
+                element.appendChild(hecho);
+            });
 
-let list = [] // To save persons
-
-// To Add 10 fake persons
-for (let i = 0; i < 10; i++) {
-    list.push(createPerson(`Name ${i}`, `Lastname ${i}`, cumpleanosFormato(i) , `000000${i}`));
-}
-
-console.table(list);
-
-// Array Higher function
-// Show only the name persons
-console.table(list.map(({
-    name
-}) => name));
-
-// Get average age
-/*
-console.log("Average age %i", list.reduce((sum, {
-    birthday
-}) => {
-    var ageDifMs = Date.now() - birthday;
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970) + sum;
-}, 0) / list.length);
-*/
-
-// More readable
-
-function getAge(birthday) {
-    var fecha_actual = new Date(Date.now());
-    var ageDifMs = fecha_actual - birthday + contarAnoBiciesto();
-
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    var ano = Math.abs(ageDate.getUTCFullYear() - 1970);
-
-    var mes_cumpleanos = Math.abs(ageDate.getUTCMonth());
-    var mes_actual = fecha_actual.getUTCMonth();
-
-    var dia_cumpleanos = Math.abs(ageDate.getUTCDay());
-    var dia_actual = fecha_actual.getUTCDay();
-    
-    if(mes_actual==mes_cumpleanos){
-        if(dia_actual>dia_cumpleanos){
-            ano=ano-1;
-        }
-    }else if(mes_actual>mes_cumpleanos){
-        ano=ano-1;
-    }
-    return ano;
-}
-
-function contarAnoBiciesto() {
-    let anio_inicial =1;
-    let anio_actual = 1970;
-    //let anio_actual = (new Date(Date.now())).getUTCFullYear();
-    for (var cont = 0; anio_inicial <= anio_actual; anio_inicial++) {
-        if((anio_inicial%4)===0 && (anio_inicial%100)!=0 || (anio_inicial%400)==0){
-            cont=cont+1;
+            if (priority) {
+                this.listTask.unshift({
+                    element,
+                    task
+                });
+                this.listHTML.insertBefore(element, this.listHTML.childNodes[0]);
+            } else {
+                this.listTask.push({
+                    element,
+                    task
+                });
+                this.listHTML.appendChild(element);
+            }
         }
     }
-    //console.log(cont);
-    return (cont)*24*60*60*1000;
+
+    let form = document.managerTask;
+    form.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        let task = form.task.value;
+
+        let validTask = /.{2,}/;
+        if (!validTask.test(task)) {
+            console.log("Ingrese una descripcion clara");
+            return false;
+        }
+
+        todoList.add(task, form.important.checked);
+
+    });
 }
-
-function cumpleanosFormato(ite) {
-    let cumpleanos = new Date().setFullYear(1990 + ite + Math.floor(Math.random() * 5));
-
-    return cumpleanos;
-}
-
-console.log("Average age %i", list.reduce((sum, {
-    birthday
-}) => getAge(birthday) + sum, 0) / list.length);
